b57628a4e724106682afc249c2f91c1594ab80aa
Sara Grassi <sara.grassi@heig-vd.ch>
2023-03-16 02:34:18 +0100

    Part 1 Step 2

--- src/main/scala/Utils/SpellCheckerService.scala
+++ src/main/scala/Utils/SpellCheckerService.scala
@@ -1,33 +1,79 @@
 package Utils
 
 trait SpellCheckerService:
-  /**
-    * This dictionary is a Map object that contains valid words as keys and their normalized equivalents as values (e.g.
-    * we want to normalize the words "veux" and "aimerais" in one unique term: "vouloir").
+  /** This dictionary is a Map object that contains valid words as keys and
+    * their normalized equivalents as values (e.g. we want to normalize the
+    * words "veux" and "aimerais" in one unique term: "vouloir").
     */
   val dictionary: Map[String, String]
 
-  /**
-    * Calculate the Levenstein distance between two words.
-    * @param s1 the first word
-    * @param s2 the second word
-    * @return an integer value, which indicates the Levenstein distance between "s1" and "s2"
+  /** Calculate the Levenstein distance between two words.
+    * @param s1
+    *   the first word
+    * @param s2
+    *   the second word
+    * @return
+    *   an integer value, which indicates the Levenstein distance between "s1"
+    *   and "s2"
     */
   def stringDistance(s1: String, s2: String): Int
 
-  /**
-    * Get the syntactically closest word in the dictionary from the given misspelled word, using the "stringDistance"
-    * function. If the word is a number or a pseudonym, this function just returns it.
-    * @param misspelledWord the mispelled word to correct
-    * @return the closest normalized word from "mispelledWord"
+  /** Get the syntactically closest word in the dictionary from the given
+    * misspelled word, using the "stringDistance" function. If the word is a
+    * number or a pseudonym, this function just returns it.
+    * @param misspelledWord
+    *   the mispelled word to correct
+    * @return
+    *   the closest normalized word from "mispelledWord"
     */
   def getClosestWordInDictionary(misspelledWord: String): String
 end SpellCheckerService
 
-class SpellCheckerImpl(val dictionary: Map[String, String]) extends SpellCheckerService:
+class SpellCheckerImpl(val dictionary: Map[String, String])
+    extends SpellCheckerService:
   // TODO - Part 1 Step 2
-  def stringDistance(s1: String, s2: String): Int = ???
+  def stringDistance(str1: String, str2: String): Int =
+    val len1 = str1.length
+    val len2 = str2.length
+    val matrix = Array.ofDim[Int](len1 + 1, len2 + 1)
+    for i <- 0 to len1 do matrix(i)(0) = i
+    for j <- 0 to len2 do matrix(0)(j) = j
+    for i <- 1 to len1; j <- 1 to len2 do
+      val cost = if (str1(i - 1) == str2(j - 1)) 0 else 1
+      matrix(i)(j) = math.min(
+        math.min(matrix(i - 1)(j) + 1, matrix(i)(j - 1) + 1),
+        matrix(i - 1)(j - 1) + cost
+      )
+    matrix(len1)(len2)
+  end stringDistance
+  // TODO - Part 1 Step 2
 
-  // TODO - Part 1 Step 2
-  def getClosestWordInDictionary(misspelledWord: String): String = ???
+  def getDistancesToDictionary(misspelledWord: String): List[(Int, String)] =
+    dictionary.keys.toList.map(word =>
+      (stringDistance(misspelledWord, word), word)
+    )
+  end getDistancesToDictionary
+
+  def getClosestWordInDictionary(misspelledWord: String): String =
+    // Check if the word is a number
+    if (misspelledWord.forall(_.isDigit)) {
+      return misspelledWord
+    }
+    // Check if the word is a pseudonym
+    if (misspelledWord.startsWith("_")) {
+      return misspelledWord
+    }
+    // return the normalized word if the word is in the dictionary
+    if (dictionary.contains(misspelledWord)) {
+      return dictionary(misspelledWord)
+    }
+    // find the key with the lowest score
+    // if more than one, get the lowest key
+    val lst = getDistancesToDictionary(misspelledWord)
+    val (minKey, minValue) = lst.minBy(_._1)
+    val key = lst.filter(_._1 == minKey).sortBy(_._2).head._2
+    return dictionary(key)
+
+  end getClosestWordInDictionary
+
 end SpellCheckerImpl
--- src/test/scala/BotTenderTokenizerInputSuite.scala
+++ src/test/scala/BotTenderTokenizerInputSuite.scala
@@ -28,19 +28,68 @@ class BotTenderTokenizerInputSuite
     evaluateInput("quitter")
   }
 
-  // You can use this test to debug any input
+  // You can use this to debug Utils.ClinksCalculator.factorial
   property("test-factorial") {
     ClinksCalculator.factorial(3) should equal(6)
     ClinksCalculator.factorial(10) should equal(3628800)
   }
 
-  // You can use this test to debug any input
+  // You can use this to debug Utils.ClinksCalculator.calculateCombination
   property("test-calculate-combination") {
     ClinksCalculator.calculateCombination(2, 2) should equal(1)
     ClinksCalculator.calculateCombination(3, 2) should equal(3)
     ClinksCalculator.calculateCombination(4, 2) should equal(6)
     ClinksCalculator.calculateCombination(5, 2) should equal(10)
     ClinksCalculator.calculateCombination(6, 2) should equal(15)
+  }
+
+  // You can use this to debug Utils.spellCheckerSvc.stringDistance
+  property("test-levenshtein") {
+    spellCheckerSvc.stringDistance("diète", "diète") should equal(0)
+    spellCheckerSvc.stringDistance("bière", "bière") should equal(0)
+    spellCheckerSvc.stringDistance("dièto", "bière") should equal(3)
+    // identical
+    spellCheckerSvc.stringDistance("hello", "hello") should equal(0)
+    // same length and differ by one character
+    spellCheckerSvc.stringDistance("kitten", "sitten") should equal(1)
+    // same length and differ by multiple characters
+    spellCheckerSvc.stringDistance("saturday", "sunday") should equal(3)
+    // one string is a prefix of the other:
+    spellCheckerSvc.stringDistance("stack", "stacks") should equal(1)
+    // ne string is empty:
+    spellCheckerSvc.stringDistance("hello", "") should equal(5)
+    // both strings are empty:
+    spellCheckerSvc.stringDistance("", "") should equal(0)
+  }
+
+  // You can use this to debug Utils.spellCheckerSvc.getClosestWordInDictionary
+  property("test-get-closest-word") {
+    // Test case 1: Word is in the dictionary
+    spellCheckerSvc.getClosestWordInDictionary("bonjour") should equal(
+      "bonjour"
+    )
+    spellCheckerSvc.getClosestWordInDictionary("hello") should equal(
+      "bonjour"
+    )
+    // Test case 2: Word is misspelled but similar to a word in the dictionary
+    spellCheckerSvc.getClosestWordInDictionary("bionjour") should equal(
+      "bonjour"
+    )
+    // Test case 3: Word is misspelled and not similar to any word in the dictionary
+    spellCheckerSvc.getClosestWordInDictionary("aloha") should equal("ou")
+    // Test case 4: Word is a number
+    spellCheckerSvc.getClosestWordInDictionary("123") should equal("123")
+    // Test case 5: Word is a pseudonym
+    spellCheckerSvc.getClosestWordInDictionary("_sara") should equal("_sara")
+    // Test case 6: Word is a single character
+    spellCheckerSvc.getClosestWordInDictionary("j") should equal("je")
+    // Test case 8: Word is plural
+    spellCheckerSvc.getClosestWordInDictionary("croissants") should equal(
+      "croissant"
+    )
+    // Test case 9: Word has accent marks
+    spellCheckerSvc.getClosestWordInDictionary("bière") should equal("biere")
+    spellCheckerSvc.getClosestWordInDictionary("diète") should equal("biere")
   }
 
   property("inputting 'quitter'") {
@@ -55,4 +104,5 @@ class BotTenderTokenizerInputSuite
   property("inputting 'santé !'") {
     evaluateInput("santé !") should equal(true)
   }
+
 }
