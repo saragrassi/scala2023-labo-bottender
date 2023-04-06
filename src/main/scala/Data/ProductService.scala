48dd1810c042e4979b86c8336b43236629e9a091
Sara Grassi <sara.grassi@heig-vd.ch>
2023-04-07 01:20:33 +0200

    Part 2 Step 2

--- src/main/scala/Data/ProductService.scala
+++ src/main/scala/Data/ProductService.scala
@@ -4,11 +4,66 @@ trait ProductService:
   type BrandName = String
   type ProductName = String
 
+  /** Get the price of a product for a given brand
+    *
+    * @param product
+    *   the product name
+    * @param brand
+    *   the brand name
+    * @return
+    *   the price of the product for the given brand
+    */
   def getPrice(product: ProductName, brand: BrandName): Double
+
+  /** Get the default price of a product, using its default brand
+    *
+    * @param product
+    *   the product name
+    * @return
+    *   the price of the product for its default brand
+    */
+  def getPrice(product: ProductName): Double
+
   def getDefaultBrand(product: ProductName): BrandName
 
 class ProductImpl extends ProductService:
   // TODO - Part 2 Step 2
-  def getPrice(product: ProductName, brand: String): Double = ???
-  def getDefaultBrand(product: ProductName): BrandName = ???
+  private val products: Map[ProductName, Map[BrandName, Double]] = Map(
+    "biere" -> Map(
+      "boxer" -> 1.0,
+      "farmer" -> 1.0,
+      "wittekop" -> 2.0,
+      "punkipa" -> 3.0,
+      "jackhammer" -> 3.0,
+      "tenebreuse" -> 4.0
+    ),
+    "croissant" -> Map(
+      "maison" -> 2.0,
+      "cailler" -> 2.0
+    )
+  )
+
+  def getPrice(product: ProductName, brand: String): Double =
+    products
+      .getOrElse(
+        product,
+        throw new IllegalArgumentException(s"Unknown product $product")
+      )
+      .getOrElse(
+        brand,
+        throw new IllegalArgumentException(s"Unknown brand $brand")
+      )
+  def getPrice(product: ProductName): Double =
+    products
+      .getOrElse(
+        product,
+        throw new IllegalArgumentException(s"Unknown product $product")
+      )
+      .getOrElse(getDefaultBrand(product), 0.0)
+
+  def getDefaultBrand(product: ProductName): BrandName = product match
+    case "biere"     => "boxer"
+    case "croissant" => "maison"
+    case _ => throw new IllegalArgumentException(s"Unknown product $product")
+
 end ProductImpl
